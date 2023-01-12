14e5bef89a98fdea79b8617d79cca393339b7ffe
masdefry <muhammaddefryan@gmail.com>
2023-01-12 16:18:07 +0700

    Day-13 Done

--- uploadfilesystem-api/index.js
+++ uploadfilesystem-api/index.js
@@ -1,6 +1,8 @@
 const express = require('express')
+const cors = require('cors') 
 const app = express()
 app.use(express.json()) // Initialize body parser ---> Untuk menerima request.body dari frontend
+app.use(cors())
 
 const PORT = 5000
 
--- uploadfilesystem-app/src/Components/CreateModal.jsx
+++ uploadfilesystem-app/src/Components/CreateModal.jsx
@@ -1,9 +1,56 @@
-import { useState } from "react"
+import { useState, useRef } from "react"
 import {Modal, ModalBody} from 'reactstrap';
+import axios from "axios";
 
 export default function CreateModal(){
 
+    let name = useRef()
+    let price = useRef()
+    let stocks = useRef()
+
     const[modalOpen, setModalOpen] = useState(false)
+    const[message, setMessage] = useState('')
+    const[selectedImages, setSelectedImages] = useState([])
+
+    let onImagesValidation = (e) => {
+        try {
+            let files = [...e.target.files] // [{}, {}, {}]
+
+            // Validasi total file
+            if(files.length > 3) throw { message: 'Select 3 Images or Less!' }
+            
+            // Validasi filesize 
+            files.forEach((value) => {
+                if(value.size > 100000) throw { message: `${value.name} more than 100Kb` }
+            })
+
+            setSelectedImages(files)
+            setMessage('')
+        } catch (error) {
+            setMessage(error.message)
+        }
+    }
+
+    let onSubmit = async() => {
+        try {
+            // Step-1 Ambil inputan dari user
+            let inputName = name.current.value 
+            let inputPrice = price.current.value 
+            let inputStocks = stocks.current.value 
+            
+            let fd = new FormData()
+            fd.append('data', JSON.stringify({name: inputName, price: inputPrice, stocks: inputStocks}))
+            selectedImages.forEach(value => {
+                fd.append('images', value)
+            })
+            
+            await axios.post(`http://localhost:5000/products/create`, fd)
+            
+            alert('Upload Success')
+        } catch (error) {
+            console.log(error)
+        }
+    }
 
     return(
         <>
@@ -17,15 +64,15 @@ export default function CreateModal(){
                     </div>
                     <div className="px-3 py-3">
                         <h6>Product Name :</h6>
-                        <input type="text" className="form-control" />
+                        <input type="text" ref={name} className="form-control" />
                     </div>
                     <div className="px-3 py-3">
                         <h6>Price :</h6>
-                        <input type="text" className="form-control" />
+                        <input type="text" ref={price} className="form-control" />
                     </div>
                     <div className="px-3 py-3">
                         <h6>Stock :</h6>
-                        <input type="text" className="form-control" />
+                        <input type="text" ref={stocks} className="form-control" />
                     </div>
                     <div className="px-3 pt-3">
                         <h6>Select Images :</h6>
@@ -33,13 +80,16 @@ export default function CreateModal(){
                     <div className="row border mx-3 px-3 py-3 rounded">
                         <div className="col-12">
                             <div>
-                                <input type="file" accept="image/*" multiple onChange={(e) => this.onImagesValidation(e)} />
+                                <input type="file" accept="image/*" multiple onChange={(e) => onImagesValidation(e)} />
                                 {/* <input type="button" value="Choose File" onClick={() => this.files.click()} className="btn btn-warning" /> */}
                             </div>
                         </div>
                     </div>
+                    <div>
+                        {message}
+                    </div>
                     <div className="my-3 px-3 py-3">
-                        <input type="button" value="Submit Data" className="btn btn-primary w-100" />
+                        <input type="button" onClick={onSubmit} value="Submit Data" className="btn btn-primary w-100" />
                     </div>
                 </ModalBody>
             </Modal>
