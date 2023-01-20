59c6927e25b95299e9a6861761eaf99f1d908b12
masdefry <muhammaddefryan@gmail.com>
2023-01-20 16:10:50 +0700

    Day-18 Done

--- bus-api/controllers/busController.js
+++ bus-api/controllers/busController.js
@@ -11,6 +11,33 @@ const transaction_details = db.transaction_details
 
 module.exports = {
     search: async(req, res) => {
-        
+        try {
+            let {date, from, to} = req.query 
+            console.log(date)
+            console.log(from)
+            console.log(to)
+
+            let findBusBy = await sequelize.query(`SELECT b.id, b.name, br.from, br.to, t.schedule_date, br.class, br.price, br.total_seat, IFNULL(SUM(t.total_seat), 0) AS total_seat_booked, 
+                br.total_seat - IFNULL(SUM(t.total_seat), 0) AS total_seat_available FROM transactions t 
+                RIGHT JOIN bus_rutes br ON (br.id = t.bus_rute_id AND (t.schedule_date = ?))
+                JOIN buses b ON b.id = br.bus_id
+                WHERE br.from = ? AND br.to = ?
+                GROUP BY b.id;`,
+                {
+                    replacements: [date, from, to], 
+                    type: sequelize.QueryTypes.SELECT
+                } 
+            )
+
+            console.log(findBusBy)
+
+            res.status(200).send({
+                isError: false, 
+                message: 'Search Bus Success', 
+                data: findBusBy
+            })
+        } catch (error) {
+            
+        }
     }
 }   
--- bus-app/src/pages/buslist.jsx
+++ bus-app/src/pages/buslist.jsx
@@ -1,18 +1,46 @@
+import {useLocation} from 'react-router-dom';
+
+import axios from 'axios';
+import { useEffect } from 'react';
+import { useState } from 'react';
+
 export default function BusList(){
+
+    let query = useLocation().search.slice(0, -2)
+    console.log(query)
+    
+    const [data, setData] = useState([])
+
+    let search = async() => {
+        try {
+            let result = await axios.get(`http://localhost:5004/bus/search${query}`)
+            setData(result.data.data)
+        } catch (error) {
+            console.log(error)
+        }
+    }
+
+    useEffect(() => {
+        search()
+    }, [])
+
     return(
         <>
             <div className="container px-5 py-5">
-                <div className="border mb-2">
+                {
+                    data.map((value, index) => {
+                        return(
+                            <div className="border mb-2">
                     <div className="row pl-5">
                         <div className="col-4 row">
                             <div className="col-12">
                                 <h3>
-                                    Medali Mas
+                                    {value.name}
                                 </h3>
                             </div>
                             <div className="col-12">
                                 <h6 className="font-weight-light">
-                                    Executive Class
+                                    {value.class}
                                 </h6>
                             </div>
                         </div>
@@ -34,7 +62,7 @@ export default function BusList(){
                             </div>
                             <div className="col-4">
                                 <h6>
-                                    Bandung
+                                    {value.from}
                                 </h6>
                             </div>
                             <div className="col-4 text-center">
@@ -44,22 +72,22 @@ export default function BusList(){
                             </div>
                             <div className="col-4">
                                 <h6>
-                                    Tangerang Selatan
+                                    {value.to}
                                 </h6>
                             </div>
                         </div>
                         <div className="col-4 text-right">
                             <div className="col-12">
                                 <h3>
-                                    Rp. 145.000
+                                    Rp. {value.price.toLocaleString()}
                                 </h3>
                             </div>
                             <div className="col-12">
                                 <h6 className="font-weight-light">
-                                    20 Seat Tersedia
+                                    {value.total_seat_booked} Booked from {value.total_seat_available}
                                 </h6>
                                 <div class="progress">
-                                    <div class="progress-bar bg-success" role="progressbar" aria-valuenow="25" aria-valuemin="0" aria-valuemax="100" style={{width: '25%'}}></div>
+                                    <div class="progress-bar bg-danger" role="progressbar"  style={{width: `${value.total_seat_booked / value.total_seat * 100}%`}}></div>
                                 </div>
                             </div>
                             <div className="col-12">
@@ -68,138 +96,9 @@ export default function BusList(){
                         </div>
                     </div>
                 </div>
-                <div className="border mb-2">
-                    <div className="row pl-5">
-                        <div className="col-4 row">
-                            <div className="col-12">
-                                <h3>
-                                    Medali Mas
-                                </h3>
-                            </div>
-                            <div className="col-12">
-                                <h6 className="font-weight-light">
-                                    Executive Class
-                                </h6>
-                            </div>
-                        </div>
-                        <div className="col-4 row">
-                            <div className="col-4">
-                                <h5>
-                                    14:00
-                                </h5>
-                            </div>
-                            <div className="col-4 text-center">
-                                <h6 className="font-weight-light">
-                                    14j 50m
-                                </h6>
-                            </div>
-                            <div className="col-4">
-                                <h5>
-                                    04:00
-                                </h5>
-                            </div>
-                            <div className="col-4">
-                                <h6>
-                                    Bandung
-                                </h6>
-                            </div>
-                            <div className="col-4 text-center">
-                                <h6 className="font-weight-light">
-                                    -
-                                </h6>
-                            </div>
-                            <div className="col-4">
-                                <h6>
-                                    Tangerang Selatan
-                                </h6>
-                            </div>
-                        </div>
-                        <div className="col-4 text-right">
-                            <div className="col-12">
-                                <h3>
-                                    Rp. 145.000
-                                </h3>
-                            </div>
-                            <div className="col-12">
-                                <h6 className="font-weight-light">
-                                    20 Seat Tersedia
-                                </h6>
-                                <div class="progress">
-                                    <div class="progress-bar bg-success" role="progressbar" aria-valuenow="25" aria-valuemin="0" aria-valuemax="100" style={{width: '25%'}}></div>
-                                </div>
-                            </div>
-                            <div className="col-12">
-                                <button type="submit" class="btn btn-danger w-20 mt-3 mb-3">Book Seat</button>
-                            </div>
-                        </div>
-                    </div>
-                </div>
-                <div className="border mb-2">
-                    <div className="row pl-5">
-                        <div className="col-4 row">
-                            <div className="col-12">
-                                <h3>
-                                    Medali Mas
-                                </h3>
-                            </div>
-                            <div className="col-12">
-                                <h6 className="font-weight-light">
-                                    Executive Class
-                                </h6>
-                            </div>
-                        </div>
-                        <div className="col-4 row">
-                            <div className="col-4">
-                                <h5>
-                                    14:00
-                                </h5>
-                            </div>
-                            <div className="col-4 text-center">
-                                <h6 className="font-weight-light">
-                                    14j 50m
-                                </h6>
-                            </div>
-                            <div className="col-4">
-                                <h5>
-                                    04:00
-                                </h5>
-                            </div>
-                            <div className="col-4">
-                                <h6>
-                                    Bandung
-                                </h6>
-                            </div>
-                            <div className="col-4 text-center">
-                                <h6 className="font-weight-light">
-                                    -
-                                </h6>
-                            </div>
-                            <div className="col-4">
-                                <h6>
-                                    Tangerang Selatan
-                                </h6>
-                            </div>
-                        </div>
-                        <div className="col-4 text-right">
-                            <div className="col-12">
-                                <h3>
-                                    Rp. 145.000
-                                </h3>
-                            </div>
-                            <div className="col-12">
-                                <h6 className="font-weight-light">
-                                    20 Seat Tersedia
-                                </h6>
-                                <div class="progress">
-                                    <div class="progress-bar bg-success" role="progressbar" aria-valuenow="25" aria-valuemin="0" aria-valuemax="100" style={{width: '25%'}}></div>
-                                </div>
-                            </div>
-                            <div className="col-12">
-                                <button type="submit" class="btn btn-danger w-20 mt-3 mb-3">Book Seat</button>
-                            </div>
-                        </div>
-                    </div>
-                </div>
+                        )
+                    })
+                }
             </div>
         </>
     )
