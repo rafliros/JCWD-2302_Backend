780d153f0adc96780781d5748c8fb4a7b7c4f718
masdefry <muhammaddefryan@gmail.com>
2023-01-26 14:35:36 +0700

    Review Raja Ongkir

--- challenge-app/src/App.js
+++ challenge-app/src/App.js
@@ -88,102 +88,84 @@ import './App.css'
 
 function App(){
 
-    const [provinceOrigin, setProvinceOrigin] = useState([])
+    const [province, setProvince] = useState([])
     const [cityOrigin, setCityOrigin] = useState([])
-    const [provinceDestination, setProvinceDestination] = useState([])
     const [cityDestination, setCityDestination] = useState([])
-    const [dataToFind, setDataToFind] = useState(
-        {
-            cityOrigin_id: null, 
-            cityOrigin_name: '', 
-            cityDestination_id: null, 
-            cityDestination_name: '', 
-            courier: ''
-        }
-    )
-    const [serviceList, setServiceList] = useState([])
-    const [loading, setLoading] = useState(false)
+    const [selectedData, setSelectedData] = useState({
+        origin_id: '',
+        origin_name: '',
+        destination_id: '',
+        destination_name: '', 
+        weight: 1700, 
+        courier: 'jne'
+    })
 
-    let onGetProvince = async() => {
+    let getDataProvince = async() => {
         try {
-            let response = await axios.get('https://jcwd-s-challenge-raja-ongkir-api.vercel.app/rajaongkir/get-province', 
-                {}, {
-                headers: {
-                    'key': ''
-                }
-            })
-            setProvinceOrigin(response.data.data.rajaongkir.province)
-            setProvinceDestination(response.data.data.rajaongkir.province)
+            let response = await axios.get(`https://s-challenge-raja-ongkir-api.vercel.app/rajaongkir/get-province`,
+                {headers: {
+                    'key': '598395fbbd5364b73d2c50d57df09682'
+                }}
+            )
+            setProvince(response.data.data.rajaongkir.province)
         } catch (error) {
             
         }
     }
 
-    let onGetCityOrigin = async(province) => {
+    let getDataCity = async(type, value) => {
         try {
-            let response = await axios.get(`https://jcwd-s-challenge-raja-ongkir-api.vercel.app/rajaongkir/get-city?province_id=${province}`)
+            let response = await axios.get(`https://jcwd-s-challenge-raja-ongkir-api.vercel.app/rajaongkir/get-city?province_id=${value}`,
+            {headers: {
+                'key': '598395fbbd5364b73d2c50d57df09682'
+            }})
             
-            setCityOrigin(response.data.data.rajaongkir.city)
+            if(type === 'origin'){
+                setCityOrigin(response.data.data.rajaongkir.city)
+            }else{
+                setCityDestination(response.data.data.rajaongkir.city)
+            }
         } catch (error) {
             console.log(error)
         }
     }
 
-    let onGetCityDestination = async(province) => {
-        try {
-            console.log(province)
-            let response = await axios.get(`https://jcwd-s-challenge-raja-ongkir-api.vercel.app/rajaongkir/get-city?province_id=${province}`)
-           setCityDestination(response.data.data.rajaongkir.city)
-        } catch (error) {
-            console.log(error)
-        }
-    }
+    let onSelectCity = (type, valll) => {
+        let newSelectedData = {...selectedData}
 
-    let onSelectCity = (type, city) => {
-        let newDataToFind = {...dataToFind}
         if(type === 'origin'){
-            newDataToFind.cityOrigin_id = city.split('&')[0]
-            newDataToFind.cityOrigin_name = city.split('&')[1]
-        }else if(type === 'destination'){
-            newDataToFind.cityDestination_id = city.split('&')[0]
-            newDataToFind.cityDestination_name = city.split('&')[1]
+            newSelectedData.origin_id = valll.split('&')[0]
+            newSelectedData.origin_name = valll.split('&')[1]
+        }else{
+            newSelectedData.destination_id = valll.split('&')[0]
+            newSelectedData.destination_name = valll.split('&')[1]
         }
-        setDataToFind(newDataToFind)
+
+        setSelectedData(newSelectedData)
     }
 
-    let onSelectCourier = async(courier) => {
+    let onSubmit = async() => {
         try {
-            let newDataToFind = {...dataToFind}
-            newDataToFind.courier = courier
-            setDataToFind(newDataToFind)
-        } catch (error) {
-            
-        }
-    }
-
-    let onGetCost = async() => {
-        try {
-            let origin = dataToFind.cityOrigin_id
-            let destination = dataToFind.cityDestination_id
-            let courier = dataToFind.courier
-            let weight = 1000
-            setLoading(true)
-
-            let response = await axios.post(`
-                https://jcwd-s-challenge-raja-ongkir-api.vercel.app/rajaongkir/get-shipping-cost
-            `, {origin, destination, weight, courier})
-
-            setServiceList(response.data.data.rajaongkir.shipping_cost.cost[0].costs)
-            setLoading(false)
+            let response = await axios.post(`https://jcwd-s-challenge-raja-ongkir-api.vercel.app/rajaongkir/get-shipping-cost`,
+            {
+                origin: selectedData.origin_id,
+                destination: selectedData.destination_id, 
+                weight: 1700, 
+                courier: selectedData.courier
+            },
+            {headers: {
+                'key': '598395fbbd5364b73d2c50d57df09682'
+            }})
+            console.log(response)
         } catch (error) {
             
         }
     }
 
     useEffect(() => {
-        onGetProvince()
+        getDataProvince()
     }, [])
-
+ 
     return(
         <>
             <div className="container px-5 py-5">
@@ -198,12 +180,12 @@ function App(){
                         <h5>
                             Origin:
                         </h5>
-                        <select onChange={(e) => onGetCityOrigin(e.target.value)} class="form-control form-control-lg">
+                        <select onChange={(e) => getDataCity('origin', e.target.value)} class="form-control form-control-lg">
                             <option>Select Province</option>
                             {
-                                provinceOrigin.map((value, index) => {
+                                province.map((value, index) => {
                                     return(
-                                        <option value={value.province_id}>{value.province}</option>
+                                        <option key={index} value={value.province_id}>{value.province}</option>
                                     )
                                 })
                             }
@@ -215,48 +197,49 @@ function App(){
                                     {
                                         cityOrigin.map((value, index) => {
                                             return(
-                                                <option value={`${value.city_id}&${value.city_name}`}>{value.city_name}</option>
+                                                <option key={index} value={`${value.city_id}&${value.city_name}`}>{value.city_name}</option>
                                             )
                                         })
                                     }
                                 </select>
                             :
-                                    null
+                                null
                         }
 
                         <h5 className="mt-3">
                             Destination:
                         </h5>
-                        <select onChange={(e) => onGetCityDestination(e.target.value)} class="form-control form-control-lg">
+                        <select onChange={(e) => getDataCity('destination', e.target.value)} class="form-control form-control-lg">
                             <option>Select Province</option>
                             {
-                                provinceDestination.map((value, index) => {
+                                province.map((value, index) => {
                                     return(
-                                        <option value={value.province_id}>{value.province}</option>
+                                        <option key={index} value={value.province_id}>{value.province}</option>
                                     )
                                 })
                             }
                         </select>
-                        {
+                        
+                            {
                             cityDestination.length > 0?
                                 <select onChange={(e) => onSelectCity('destination', e.target.value)} class="form-control form-control-lg mt-3">
                                     <option>Select City</option>
                                     {
                                         cityDestination.map((value, index) => {
                                             return(
-                                                <option value={`${value.city_id}&${value.city_name}`}>{value.city_name}</option>
+                                                <option key={index} value={`${value.city_id}&${value.city_name}`}>{value.city_name}</option>
                                             )
                                         })
                                     }
                                 </select>
                             :
-                                    null
+                                null
                         }
 
                         <h5 className="mt-3">
                             Courier:
                         </h5>
-                        <select onChange={(e) => onSelectCourier(e.target.value)} class="form-control form-control-lg">
+                        <select class="form-control form-control-lg">
                             <option>Select Courier</option>
                             <option value='jne'>JNE</option>
                         </select>
@@ -269,77 +252,44 @@ function App(){
                             <tbody>
                                 <tr>
                                     <th scope="row" className="border-right-0 w-25">Origin</th>
-                                    <td className="border-left-0">: {dataToFind.cityOrigin_name}</td>
+                                    <td className="border-left-0">: {selectedData.origin_name}</td>
                                 </tr>
                                 <tr>
                                     <th scope="row" className="border-right-0 w-25">Destination</th>
-                                    <td className="border-left-0">: {dataToFind.cityDestination_name}</td>
+                                    <td className="border-left-0">: {selectedData.destination_name}</td>
                                 </tr>
                                 <tr>
                                     <th scope="row" className="border-right-0 w-25">Courier</th>
-                                    <td className="border-left-0">: {dataToFind.courier}</td>
+                                    <td className="border-left-0">: jne</td>
                                 </tr>
                             </tbody>
                         </table>
                         
-                        <button onClick={onGetCost} className="btn btn-primary w-100 rounded-0">
+                        <button onClick={onSubmit} className="btn btn-primary w-100 rounded-0">
                             Cek Ongkir
                         </button>
                     </div>
                     <div className="col-12 mt-5">
-                        {
-                            loading === false?
-                            <table className="table table-bordered">
-                                <thead>
-                                    <tr>
-                                    <th scope="col">No</th>
-                                    <th scope="col">Service Name</th>
-                                    <th scope="col">Description</th>
-                                    <th scope="col">Estimation</th>
-                                    <th scope="col">Ongkir</th>
-                                    </tr>
-                                </thead>
-                                <tbody>
-                                    {
-                                        serviceList.map((value, index)=> {
-                                            return(
-                                                <tr>
-                                                    <th scope="row">{index+1}</th>
-                                                    <td>{value.service}</td>
-                                                    <td>{value.description}</td>
-                                                    <td>{value.cost[0].etd} Days</td>
-                                                    <td>{value.cost[0].value}</td>
-                                                </tr>
-                                            )
-                                        })
-                                    }
-                                </tbody>
-                            </table>
-                            :
-                            <>
-                                <div class="spinner-grow text-primary" role="status">
-  <span class="sr-only">Loading...</span>
-</div>
-<div class="spinner-grow text-secondary" role="status">
-  <span class="sr-only">Loading...</span>
-</div>
-<div class="spinner-grow text-success" role="status">
-  <span class="sr-only">Loading...</span>
-</div>
-<div class="spinner-grow text-danger" role="status">
-  <span class="sr-only">Loading...</span>
-</div>
-<div class="spinner-grow text-warning" role="status">
-  <span class="sr-only">Loading...</span>
-</div>
-<div class="spinner-grow text-info" role="status">
-  <span class="sr-only">Loading...</span>
-</div>
-<div class="spinner-grow text-light" role="status">
-  <span class="sr-only">Loading...</span>
-</div>
-                            </>
-                        }
+                    <table className="table table-bordered">
+                        <thead>
+                            <tr>
+                            <th scope="col">No</th>
+                            <th scope="col">Service Name</th>
+                            <th scope="col">Description</th>
+                            <th scope="col">Estimation</th>
+                            <th scope="col">Ongkir</th>
+                            </tr>
+                        </thead>
+                        <tbody>
+                            <tr>
+                                <th scope="row"></th>
+                                <td></td>
+                                <td></td>
+                                <td>Days</td>
+                                <td></td>
+                            </tr>
+                        </tbody>
+                    </table>
                     </div>
                 </div>
             </div>
